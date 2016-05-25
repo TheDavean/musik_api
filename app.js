f785173d49c7bb5ee93b9b63de136917e198062c
TheDavean <a291647@uach.mx>
2016-05-25 10:20:04 -0600

    Start 12

--- app.js
+++ app.js
@@ -9,10 +9,17 @@ var connectionString = process.env.DATABASE_URL || 'postgres://postgres:dd197358
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //Puerto del servidor
 var port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
+
+
 
 app.get('/', function(req, res) {
 
